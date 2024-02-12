bffecc34a7cec594d16932037fb7da7de41707aa
James Hallberg <Grebllah81788@gmail.com>
2024-02-12 16:59:55 -0500

    stack lab complete

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,37 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
-
+        self.items = items
+        self.limit = limit
+        
     def isEmpty(self):
-        pass
+        if self.size() == 0:
+            return True
+        return False
 
     def push(self, item):
-        pass
-
+        if self.full() == False:
+            self.items.append(item)
+        else: return False
+        
     def pop(self):
-        pass
-
-    def peek(self):
-        pass
+        if len(self.items) > 0:
+            return self.items.pop()
+        else: return None
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        if self.size() >= self.limit:
+            return True
+        return False
 
     def search(self, target):
-        pass
+        position = self.size() - 1
+        for element in self.items:
+            if target == element:
+                return position
+            else:
+                position = position - 1
+        return -1
